0d7d6f84e7dbd67d449ea9679b0e7a040537ecb5
victorvuelma <victorvuelma@outlook.com>
2019-03-14 20:53:41 -0300

    Show 'materias' in aluno home

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('auth/cadastro', views.auth_cadastro, name='app_auth_cadastro'),
     path('auth/login', views.auth_login, name='app_auth_login'),
     path('aluno/home', views.aluno_home, name='app_aluno_home'),
+    path('aluno/home/<int:id_materia>', views.aluno_materia, name='app_aluno_materia'),
     path('professor/home', views.professor_home, name='app_professor_home'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from app.models import Materia
 
 def auth_cadastro(request):
     return render(request, 'auth_cadastro.html')
@@ -7,7 +8,21 @@ def auth_login(request):
     return render(request, 'auth_login.html')
 
 def aluno_home(request):
-    return render(request, 'aluno_home.html')
+    materias = Materia.objects.all()
+
+    return render(request, 'aluno_home.html', {
+        "materias": materias
+        })
+
+
+def aluno_materia(request, id_materia):
+    materias = Materia.objects.all()
+    materia_atual = Materia.objects.get(pk=id_materia)
+
+    return render(request, 'aluno_home.html', {
+        "materias": materias, 
+        "materia_atual": materia_atual
+        })
 
 def professor_home(request):
     return render(request, 'professor_home.html')
