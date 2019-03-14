9136ed3d3ed9c452f1cd5aa721c6f5532670a9f3
Julia Wharton <ju-wharton@live.co.uk>
2019-03-14 19:22:50 -0300

    Add views and urls

--- app/views.py
+++ app/views.py
@@ -1,1 +1,13 @@
 from django.shortcuts import render
+
+def login(request):
+    return render(request, 'login.html')
+
+def cadastro(request):
+    return render(request, 'cadastro.html')
+
+def app_aluno(request):
+    return render(request, 'app_aluno.html')
+
+def app_professor(request):
+    return render(request, 'app_professor.html')
--- landing/urls.py
+++ landing/urls.py
@@ -1,6 +1,11 @@
 from django.urls import include, path
-from . import views
+from landing.views import landing
+from app import views
 
 urlpatterns = [
-    path('', views.landing)
+    path('', landing, name='home'),
+    path('login', views.login, name='login'),
+    path('cadastro', views.cadastro, name='cadastro'),
+    path('app_aluno', views.app_aluno, name='app_aluno'),
+    path('app_professor', views.app_professor, name='app_professor'),
 ]
