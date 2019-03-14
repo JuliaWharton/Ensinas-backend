13bf1b77ad391fb6e98135802353051e4c760f82
victorvuelma <victorvuelma@outlook.com>
2019-03-13 22:11:11 -0300

    Add models and migration of app.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Materia(models.Model):
+    nome = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.nome
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=256)
+    email = models.EmailField()
+    senha = models.CharField(max_length=256)
+
+    def __str(self):
+        return self.email
+
+class Professor(models.Model):
+    nome = models.CharField(max_length=256)
+    email = models.EmailField()
+    senha = models.CharField(max_length=256)
+    aprovado = models.BooleanField(default=False)
+    materia = models.ForeignKey(Materia, on_delete=models.SET_DEFAULT, default=None)
+    curso = models.CharField(max_length=256, default=None)
+    instiuicao = models.CharField(max_length=256, default=None)
+    alunos = models.ManyToManyField(Aluno, through='Contato')
+
+    def __str(self):
+        return self.email
+
+class Contato(models.Model):
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    oculto = models.BooleanField(default=False)
