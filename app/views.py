ead203d538bd84d799186400cdd5ee381be8376b
victorvuelma <victorvuelma@outlook.com>
2019-03-14 21:57:43 -0300

    List professores based on materia

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from app.models import Materia
+from app.models import Materia, Professor
 
 def auth_cadastro(request):
     return render(request, 'auth_cadastro.html')
@@ -9,19 +9,23 @@ def auth_login(request):
 
 def aluno_home(request):
     materias = Materia.objects.all()
+    professores = Professor.objects.all()
 
     return render(request, 'aluno_home.html', {
-        "materias": materias
+        "materias": materias,
+        "professores": professores
         })
 
 
 def aluno_materia(request, id_materia):
     materias = Materia.objects.all()
     materia_atual = Materia.objects.get(pk=id_materia)
+    professores = Professor.objects.filter(materia=id_materia)
 
     return render(request, 'aluno_home.html', {
         "materias": materias, 
-        "materia_atual": materia_atual
+        "materia_atual": materia_atual,
+        "professores": professores
         })
 
 def professor_home(request):
