d019fe932a8a6e812ac84836f447ca97ae0bc007
victorvuelma <victorvuelma@outlook.com>
2019-03-17 20:59:28 -0300

    Lock solicitacoes for mentor not aproved

--- app/views/mentor_views.py
+++ app/views/mentor_views.py
@@ -9,13 +9,16 @@ def solicitacoes(request):
 	mentor = mentor_auth.get(request)
 	
 	if mentor is not None:
-		solicitacoes = Solicitacao.objects.filter(mentor=mentor, oculto=False)
+		if mentor.aprovado:
+			solicitacoes = Solicitacao.objects.filter(mentor=mentor, oculto=False)
 
-		return render(request, 'mentor_solicitacoes.html', {
-			'mentor': mentor,
-			'solicitacoes': solicitacoes
-		})
-
+			return render(request, 'mentor_solicitacoes.html', {
+				'mentor': mentor,
+				'solicitacoes': solicitacoes
+			})
+		else:
+			sweetify.error(request, 'Acesso restrito!', text='Seu cadastro ainda não foi aprovado! Por favor aguarde o contato da nossa equipe!', button='Ok', timer=5000)
+			return redirect('app_auth_login')
 	else:
 		sweetify.error(request, 'Acesso restrito!', text='Você precisa estar autenticado para acessar esta página.', button='Ok', timer=5000)
 		return redirect('app_auth_login')
